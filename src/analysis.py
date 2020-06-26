1425769d3d4a6933c0941218181fb8acbc13a316
sanchit <sa3119@ic.ac.uk>
2020-06-26 03:39:54 +0100

    fixed indexing of list calls within function

---
+++ src/analysis.py
@@ -0,0 +1,35 @@
+
+# need a function which cleverly calculates the offset
+OFFSET = 100
+AVERAGE_LENGTH = 5
+# function to calculate the simple moving average over the last n days
+
+
+def calculateSMA(historicalData):
+    length = len(historicalData)
+    return sum(historicalData[length - AVERAGE_LENGTH - 1:]) / AVERAGE_LENGTH
+
+
+def calculateOffset():
+    return
+
+    # function to calculate the exponential moving average
+    # places higher weightings on recent data
+
+
+def calculateEMA(historicalData):
+    return
+
+
+def checkPosition(historicalData, testPrice):
+    average = calculateSMA(historicalData)
+
+    if average + OFFSET < testPrice:
+        # sell
+        return -1
+    elif average - OFFSET > testPrice:
+        # buy
+        return 1
+    else:
+        # speculate
+        return 0
