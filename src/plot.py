e25cea08ce8d97c5fc0f73f6c0e1ea3336a97c10
sanchit <sa3119@ic.ac.uk>
2020-06-26 02:21:01 +0100

    minor changes

--- src/plot.py
+++ src/plot.py
@@ -1,11 +1,13 @@
 import process
 import matplotlib.pyplot as plt
 
+
 timearray = []
 
-for i in range(len(process.dataTimesPrices[1])):
+for i in range(len(process.historicalData[1])):
     timearray.append(i)
 
-plt.plot(timearray, process.dataTimesPrices[1])
+plt.plot(timearray, process.historicalData[1])
 
 plt.show()
+plt.close()
