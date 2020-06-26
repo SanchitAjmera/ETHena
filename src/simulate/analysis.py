b4bfe7eb8fe79ca26d3a98f33260172fda6e4721
sanchit <sa3119@ic.ac.uk>
2020-06-26 02:21:57 +0100

    created file for trader and analysis

---
+++ src/simulate/analysis.py
@@ -0,0 +1,23 @@
+
+AVERAGE_LENGTH = 5
+# function to calculate the simple moving average over the last n days
+
+
+def calculateSMA(historicalData):
+    length = len(historicalData)
+    return sum(historicalData[0][length - AVERAGE_LENGTH - 1:]) / AVERAGE_LENGTH
+
+
+def calculateOffset():
+
+    # function to calculate the exponential moving average
+    # places higher weightings on recent data
+
+
+def calculateEMA(historicalData):
+    return
+
+
+def checkPosition(historicalData, testPrice):
+    if calculateSMA(historicalData) >:
+        pass
---
+++ src/simulate/trade.py
@@ -0,0 +1,19 @@
+from src import process
+import analysis
+# initial variables
+
+
+class Trader:
+
+    def __init__(self, initialFund, initialInventory, initialDay, givenData, testData):
+        self.funds = initialFund
+        self.inventory = initialInventory
+        self.day = initialDay
+        self.historicalData = givenData
+        self.testData = testData
+
+    def trade():
+        if
+
+
+trader1 = Trader(100000, 0, 0, process.historicalData, process.testData)
