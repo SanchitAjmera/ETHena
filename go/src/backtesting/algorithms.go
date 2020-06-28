7862970f411660c77bf08e2f47358ff586a08141
sanchit <sanchitajmera2017@gmail.com>
2020-06-28 20:29:35 +0100

    added print dtatements only after buying or selling

--- go/src/backtesting/algorithms.go
+++ go/src/backtesting/algorithms.go
@@ -2,35 +2,45 @@ package main
 
 import (
         "strconv"
+        "fmt"
 )
 
-func SME(historicalData[] string, newData float64) float64 {
+func SME(state *state_t) (int, float64) {
 
-  n := len(historicalData)
+  newData, timeStamp:= getNextPrice(state.currentDay, state.historicalData)
+
+  if timeStamp == float64(0) {
+    return 0, newData
+  }
 	var sum float64 = 0
 
-  for i := 0; i < n; i++ {
-
-    decimalData, err := strconv.ParseFloat(historicalData[i], 8)
+  for i := 0; i < state.metrics.dataCacheLength; i++ {
+    // TODO : should moving average include the current price
+    decimalData, err := strconv.ParseFloat(state.historicalData[sheetNum][state.currentDay - i][priceCol], 8)
     if err != nil{
+      panic(err)
     }
 
     sum += decimalData
   }
-  return sum / float64(n)
-}
 
-func checkSME(state *state_t) (int, float64) {
+  average := sum / float64(state.metrics.dataCacheLength)
 
-  newData:= getNextPrice(state.currentDay, state.historicalData)
-  historicalDataSlice := state.historicalData[sheetNum][state.currentDay - state.metrics.dataCacheLength:state.currentDay][priceCol]
-  average := SME(historicalDataSlice, newData)
 
-  if average > 0 {
-    return 1, newData
-
-  } else if average < 0 {
+  if average + state.metrics.offset  <  newData {
+    fmt.Println(".")
+    fmt.Println("                   Price:          £"  , newData,
+  							"\n                   Time:          " , timeStamp)
+    fmt.Println("                   Moving Average: £",average)
     return -1, newData
+
+  } else if average - state.metrics.offset > newData {
+    fmt.Println(".")
+    fmt.Println("                   Price:          £"  , newData,
+  							"\n                   Time:          " , timeStamp)
+    fmt.Println("                   Moving Average: £",average)
+    return 1, newData
   }
-    return 0, newData
+
+  return 0, newData
 }
