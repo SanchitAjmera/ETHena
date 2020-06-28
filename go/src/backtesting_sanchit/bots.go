406c93a249c3c9f9db79bf18ce03c0ba552c37ce
sanchit <sa3119@ic.ac.uk>
2020-06-28 23:50:57 +0100

    added separate folder for sanchit's backtesting

---
+++ go/src/backtesting_sanchit/bots.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+  "fmt"
+)
+
+
+func buy(state *state_t, newPrice float64, printing bool ){
+  buyableStock := state.funds / newPrice
+
+  if buyableStock != float64(0) {
+    item := []float64{buyableStock, newPrice}
+    state.inventory = append(state.inventory, item)
+    state.funds -= buyableStock * newPrice
+  }
+
+  if printing {
+    fmt.Println("                   Bought ", buyableStock, " stocks at ",newPrice)
+  }
+
+}
+
+func sell(state *state_t, newPrice float64, printing bool) {
+  sold := float64(0)
+
+  if len(state.inventory) == 0 {
+    if printing {
+      fmt.Println("                   Sold ", sold, " stocks at ",newPrice)
+    }
+    return
+  }
+
+  for i := 0; i < len(state.inventory); i++ {
+    sold += state.inventory[i][0]
+    state.funds += state.inventory[i][0] * newPrice
+  }
+
+  state.inventory = [][]float64{}
+
+  if printing {
+    fmt.Println("                   Sold ", sold, " stocks at ",newPrice)
+  }
+
+}
+
+
+//£-3698.61 per day
+func verySimpleBot(nextPrice float64, lastPrice *float64) float64 {
+	returnVal := nextPrice - *lastPrice
+	*lastPrice = nextPrice
+	return returnVal
+}
+
+
+
+func SMEBot(state *state_t, printing bool) {
+  action, newPrice := SME(state, printing)
+
+  if action == 0 {
+    state.currentDay += state.metrics.dataCacheLength
+    return
+  }
+
+  if printing {
+    fmt.Println("                   inventory:     ",state.inventory)
+    fmt.Println("                   funds:          £",state.funds)
+  }
+
+  if action > 0 {
+    buy(state, newPrice, printing)
+  } else if action < 0{
+    sell(state, newPrice, printing)
+  }
+
+  state.currentDay += state.metrics.dataCacheLength
+}
---
+++ go/src/backtesting_sanchit/main.go
@@ -0,0 +1,20 @@
+package main
+
+import ()
+
+var priceCol int
+var timeCol int
+var sheetNum int
+
+
+func main () {
+
+  // initialising global variables
+  priceCol = 7
+  timeCol = 0
+  sheetNum = 0
+
+//	var getNextPrice ticker = tickerFunc()
+
+	tester()
+}
