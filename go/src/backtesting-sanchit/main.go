b906492f611c17fa7ab4f9c5e333df6fa169d0b1
sanchit <sa3119@ic.ac.uk>
2020-07-02 07:19:39 +0100

    finished testing stop loss multiplier for rsi bot

--- go/src/backtesting-sanchit/main.go
+++ go/src/backtesting-sanchit/main.go
@@ -35,57 +35,65 @@ func main() {
 	startingFunds := decimal.NewFromInt64(int64(100))
 	// processing historical data within excel spreadsheet
 	parseXlsx()
+
+	// initialising values within bot portfolio
+	tradingPeriod := int64(20)
+	var numOfDecisions int64 = 50000/tradingPeriod
+	stopLossMultiplier := decimal.NewFromInt64(97)
+	stopLossMultDecimal := stopLossMultiplier.Div(decimal.NewFromInt64(100),8)
+	// var offset int64 = 40
+	// initialising portfolio
+	pf := portfolio{startingFunds, decimal.NewFromInt64(int64(0)), tradingPeriod, tradingPeriod, 0, decimal.Zero(), stopLossMultDecimal}
+	// bot := smaBot{&pf, decimal.NewFromInt64(offset), numOfDecisions}
+	// initialising bot
+	bot := rsiBot{&pf, numOfDecisions, int64(30), int64(70)}
+	testRSI(&bot)
+	// calculating overall profit
+	currBid := getBid(bot.pf.currRow)
+	portfolioValue := bot.pf.funds.Add(currBid.Mul(bot.pf.stock))
+	profit := portfolioValue.Sub(startingFunds)
+	/*
 	// initialising max values
 	maxProfit := decimal.NewFromInt64(0)
-	maxTradingPeriod := int64(0)
-	maxOverSold := int64(0)
-	maxOverBought := int64(0)
-
+	maxStopLossMult := decimal.Zero()
 	// iterating through different metrics for the bot
-	for x := 0; x < 50; x+=10 {
-		for y := 100; y > 50; y-=10 {
-			i:=4
-			for i < 60{
-				// initialising values within bot portfolio
-				overSold := int64(x)
-				overBought := int64(y)
-				tradingPeriod := int64(i)
-				var numOfDecisions int64 = 50000/tradingPeriod
-				//	var offset int64 = 40
-				// initialising portfolio
-				pf := portfolio{startingFunds, decimal.NewFromInt64(int64(0)), tradingPeriod/*tradingPeriod*/, tradingPeriod/*tradingPeriod*/, 0}
-				//botSMA := smaBot{&pf, decimal.NewFromInt64(offset), numOfDecisions}
-				// initialising bot
-				bot := rsiBot{&pf, numOfDecisions, overSold, overBought}
-				testRSI(&bot)
-				// calculating overall profit
-				currBid := getBid(bot.pf.currRow)
-				portfolioValue := bot.pf.funds.Add(currBid.Mul(bot.pf.stock))
-				profit := portfolioValue.Sub(startingFunds)
-				// check to see if a greater max profit was generated
-				if profit.Cmp(maxProfit) == 1 {
-					maxProfit = profit
-					maxOverSold = bot.overSold
-					maxOverBought = bot.overBought
-					maxTradingPeriod = bot.pf.tradingPeriod
-					fmt.Println("maximum profit made: £", maxProfit)
-					fmt.Println("at trading periods:  ", maxTradingPeriod)
-					fmt.Println("upper bound: 			  ", maxOverBought)
-					fmt.Println("upper Sold:  				", maxOverSold)
-					fmt.Println(".")
-				}
-				i +=4
-			}
+	for x := 75; x < 100; x+=1 {
+		// initialising values within bot portfolio
+		tradingPeriod := int64(20)
+		var numOfDecisions int64 = 50000/tradingPeriod
+		stopLossMultiplier := decimal.NewFromInt64(int64(x))
+		stopLossMultDecimal := stopLossMultiplier.Div(decimal.NewFromInt64(100),8)
+		//	var offset int64 = 40
+		// initialising portfolio
+		pf := portfolio{startingFunds, decimal.NewFromInt64(int64(0)), tradingPeriod, tradingPeriod, 0, decimal.Zero(), stopLossMultDecimal}
+		//botSMA := smaBot{&pf, decimal.NewFromInt64(offset), numOfDecisions}
+		// initialising bot
+		bot := rsiBot{&pf, numOfDecisions, int64(30), int64(70)}
+		testRSI(&bot)
+		// calculating overall profit
+		currBid := getBid(bot.pf.currRow)
+		portfolioValue := bot.pf.funds.Add(currBid.Mul(bot.pf.stock))
+		profit := portfolioValue.Sub(startingFunds)
+		// check to see if a greater max profit was generated
+		if profit.Cmp(maxProfit) == 1 {
+			maxProfit = profit
+			maxStopLossMult = bot.pf.stopLossMult
+			//fmt.Println("maximum profit made: £", maxProfit)
+			//fmt.Println("at trading periods:  ", maxTradingPeriod)
+			//fmt.Println("upper bound: 			  ", maxOverBought)
+			//fmt.Println("upper Sold:  				", maxOverSold)
+			//fmt.Println(".")
 		}
 	}
+	*/
 	// printing out results
 	days := ((50000 / 60)/ 24)
   fmt.Println("Days: ",days)
-//	fmt.Println("Profit/Loss:     £", profit)
-  fmt.Println("maximum profit made: £", maxProfit)
-	fmt.Println(" at trading periods:  ", maxTradingPeriod)
-	fmt.Println(" 			 upper bound:  ", maxOverBought)
-	fmt.Println(" 			 upper Sold:  ", maxOverSold)
-//	fmt.Println(bot.pf.tradesMade," trades made")
+	fmt.Println("Profit/Loss:     £", profit)
+//  fmt.Println("maximum profit made: £", maxProfit)
+//	fmt.Println(" at trading periods:  ", maxTradingPeriod)
+//	fmt.Println("stop loss multiplier: ", maxStopLossMult)
+//	fmt.Println(" 			 upper Sold:  ", maxOverSold)
+	fmt.Println(bot.pf.tradesMade," trades made")
 
 }
