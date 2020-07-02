9060171050aa2579018a92a5ccb233d889555473
sanchit <sa3119@ic.ac.uk>
2020-07-02 05:58:37 +0100

    implemented testing mechanism for rsi bot metrics

--- go/src/backtesting-sanchit/main.go
+++ go/src/backtesting-sanchit/main.go
@@ -34,29 +34,44 @@ func main() {
 
 	maxProfit := decimal.NewFromInt64(0)
 	maxTradingPeriod := int64(0)
+	maxOverSold := int64(0)
+	maxOverBought := int64(0)
 
-	i:=4
+	for x := 0; x < 50; x+=10 {
+		for y := 100; y > 50; y-=10 {
+			i:=4
+			for i < 60{
+				overSold := int64(x)
+				overBought := int64(y)
+				tradingPeriod := int64(i)
+				var numOfDecisions int64 = 50000/tradingPeriod
+				//	var offset int64 = 40
 
-	for i < 121{
+				pf := portfolio{startingFunds, decimal.NewFromInt64(int64(0)), tradingPeriod/*tradingPeriod*/, tradingPeriod/*tradingPeriod*/, 0}
+				//botSMA := smaBot{&pf, decimal.NewFromInt64(offset), numOfDecisions}
+				bot := rsiBot{&pf, numOfDecisions, overSold, overBought}
+				testRSI(&bot)
+				currBid := getBid(bot.pf.currRow)
+				portfolioValue := bot.pf.funds.Add(currBid.Mul(bot.pf.stock))
+				profit := portfolioValue.Sub(startingFunds)
 
-		tradingPeriod := int64(i)
-		var numOfDecisions int64 = 50000/tradingPeriod
-	//	var offset int64 = 40
-
-		pf := portfolio{startingFunds, decimal.NewFromInt64(int64(0)), tradingPeriod/*tradingPeriod*/, tradingPeriod/*tradingPeriod*/, 0}
-		//botSMA := smaBot{&pf, decimal.NewFromInt64(offset), numOfDecisions}
-		bot := rsiBot{&pf, numOfDecisions, overSold, overBought}
-		testRSI(&bot)
-		currBid := getBid(bot.pf.currRow)
-		portfolioValue := bot.pf.funds.Add(currBid.Mul(bot.pf.stock))
-		profit := portfolioValue.Sub(startingFunds)
-
-		if profit.Cmp(maxProfit) == 1 {
-				maxProfit = profit
-				maxTradingPeriod = bot.pf.tradingPeriod
+				if profit.Cmp(maxProfit) == 1 {
+					maxProfit = profit
+					maxOverSold = bot.overSold
+					maxOverBought = bot.overBought
+					maxTradingPeriod = bot.pf.tradingPeriod
+					fmt.Println("maximum profit made: £", maxProfit)
+					fmt.Println("at trading periods:  ", maxTradingPeriod)
+					fmt.Println("upper bound: 			  ", maxOverBought)
+					fmt.Println("upper Sold:  				", maxOverSold)
+					fmt.Println(".")
+				}
+				i +=4
+			}
 		}
-		i +=2
-		}
+	}
+
+
 
 
 
@@ -65,6 +80,8 @@ func main() {
 //	fmt.Println("Profit/Loss:     £", profit)
   fmt.Println("maximum profit made: £", maxProfit)
 	fmt.Println(" at trading periods:  ", maxTradingPeriod)
+	fmt.Println(" 			 upper bound:  ", maxOverBought)
+	fmt.Println(" 			 upper Sold:  ", maxOverSold)
 //	fmt.Println(bot.pf.tradesMade," trades made")
 
 }
