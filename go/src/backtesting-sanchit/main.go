8c5ef21222aef1e2ae2c176422bdc431a1caeb3d
sanchit <sanchitajmera2017@gmail.com>
2020-07-03 04:16:48 +0100

    tested macdBot

--- go/src/backtesting-sanchit/main.go
+++ go/src/backtesting-sanchit/main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
   luno "github.com/luno/luno-go"
 	"github.com/luno/luno-go/decimal"
+	//"github.com/chenjiandongx/go-echarts/charts"
+	//"os"
 )
 
 // test function for the SMA bot
@@ -43,65 +45,84 @@ func main() {
 	startingFunds := decimal.NewFromInt64(int64(100))
 	// processing historical data within excel spreadsheet
 	parseXlsx()
-
+/*
 	// initialising values within bot portfolio
-	tradingPeriod := int64(20)
+	tradingPeriod := int64(1)
+	currRow := int64(40)
+	results := make([]decimal.Decimal, 0)
 	var numOfDecisions int64 = 50000/tradingPeriod
 	stopLossMultiplier := decimal.NewFromInt64(97)
 	stopLossMultDecimal := stopLossMultiplier.Div(decimal.NewFromInt64(100),8)
 	// var offset int64 = 40
 	// initialising portfolio
-	pf := portfolio{startingFunds, decimal.NewFromInt64(int64(0)), tradingPeriod, tradingPeriod, 0, decimal.Zero(), stopLossMultDecimal}
+	pf := portfolio{startingFunds, decimal.NewFromInt64(int64(0)), tradingPeriod, currRow, 0, decimal.Zero(), stopLossMultDecimal}
 	// bot := smaBot{&pf, decimal.NewFromInt64(offset), numOfDecisions}
 	// initialising bot
-	bot := macdBot{&pf, numOfDecisions, int64(10), int64(60)}
+	bot := macdBot{&pf, numOfDecisions, int64(10), currRow, results, decimal.Zero()}
 	testMACD(&bot)
 	// calculating overall profit
 	currBid := getBid(bot.pf.currRow)
 	portfolioValue := bot.pf.funds.Add(currBid.Mul(bot.pf.stock))
 	profit := portfolioValue.Sub(startingFunds)
-	/*
+*/
 	// initialising max values
 	maxProfit := decimal.NewFromInt64(0)
-	maxStopLossMult := decimal.Zero()
+	maxSRDuration := 0
+	maxLRDuration := 0
 	// iterating through different metrics for the bot
-	for x := 75; x < 100; x+=1 {
-		// initialising values within bot portfolio
-		tradingPeriod := int64(20)
-		var numOfDecisions int64 = 50000/tradingPeriod
-		stopLossMultiplier := decimal.NewFromInt64(int64(x))
-		stopLossMultDecimal := stopLossMultiplier.Div(decimal.NewFromInt64(100),8)
-		//	var offset int64 = 40
-		// initialising portfolio
-		pf := portfolio{startingFunds, decimal.NewFromInt64(int64(0)), tradingPeriod, tradingPeriod, 0, decimal.Zero(), stopLossMultDecimal}
-		//botSMA := smaBot{&pf, decimal.NewFromInt64(offset), numOfDecisions}
-		// initialising bot
-		bot := rsiBot{&pf, numOfDecisions, int64(30), int64(70)}
-		testRSI(&bot)
-		// calculating overall profit
-		currBid := getBid(bot.pf.currRow)
-		portfolioValue := bot.pf.funds.Add(currBid.Mul(bot.pf.stock))
-		profit := portfolioValue.Sub(startingFunds)
-		// check to see if a greater max profit was generated
-		if profit.Cmp(maxProfit) == 1 {
-			maxProfit = profit
-			maxStopLossMult = bot.pf.stopLossMult
-			//fmt.Println("maximum profit made: £", maxProfit)
-			//fmt.Println("at trading periods:  ", maxTradingPeriod)
-			//fmt.Println("upper bound: 			  ", maxOverBought)
-			//fmt.Println("upper Sold:  				", maxOverSold)
-			//fmt.Println(".")
+	for sr := 5 ; sr < 10; sr+=5 {
+		for lr := sr+5;  lr< sr+10; lr+=10 {
+
+			// initialising values within bot portfolio
+			// initialising values within bot portfolio
+			tradingPeriod := int64(1)
+
+			results := make([]decimal.Decimal, 0)
+			var numOfDecisions int64 = 50000/tradingPeriod
+			stopLossMultiplier := decimal.NewFromInt64(97)
+			stopLossMultDecimal := stopLossMultiplier.Div(decimal.NewFromInt64(100),8)
+			// var offset int64 = 40
+			// initialising portfolio
+			pf := portfolio{startingFunds, decimal.NewFromInt64(int64(0)), tradingPeriod, int64(300), 0, decimal.Zero(), stopLossMultDecimal}
+			// bot := smaBot{&pf, decimal.NewFromInt64(offset), numOfDecisions}
+			// initialising bot
+			bot := macdBot{&pf, numOfDecisions, int64(30), int64(300), results, decimal.Zero()}
+			testMACD(&bot)
+			// calculating overall profit
+			currBid := getBid(bot.pf.currRow)
+			portfolioValue := bot.pf.funds.Add(currBid.Mul(bot.pf.stock))
+			profit := portfolioValue.Sub(startingFunds)
+
+			if profit.Cmp(maxProfit) == 1 {
+				maxProfit = profit
+				maxSRDuration = 30
+				maxLRDuration = 300
+				fmt.Println("max Profit" , maxProfit)
+				fmt.Println("max SR" , maxSRDuration)
+				fmt.Println("max LR" , maxLRDuration, "\n")
+			}
 		}
 	}
-	*/
+
 	// printing out results
-	days := ((50000 / 60)/ 24)
-  fmt.Println("Days: ",days)
-	fmt.Println("Profit/Loss:     £", profit)
-//  fmt.Println("maximum profit made: £", maxProfit)
-//	fmt.Println(" at trading periods:  ", maxTradingPeriod)
-//	fmt.Println("stop loss multiplier: ", maxStopLossMult)
-//	fmt.Println(" 			 upper Sold:  ", maxOverSold)
-	fmt.Println(bot.pf.tradesMade," trades made")
+	//days := ((50000 / 60)/ 24)
+  //fmt.Println("Days: ",days)
+	//fmt.Println("Profit/Loss:     £", profit)
+	fmt.Println("max Profit" , maxProfit)
+	fmt.Println("max SR" , maxSRDuration)
+	fmt.Println("max LR" , maxLRDuration, "\n")
+//	fmt.Println(bot.pf.tradesMade," trades made")
+//	xAxis := make([]decimal.Decimal,0)
+//	for i := 0;  i < len(bot.diffs); i++ {
+//		xAxis = append(xAxis, decimal.NewFromInt64(int64(i)))
+//	}
+
+//	bar := charts.NewBar()
+//	bar.AddXAxis(xAxis).AddYAxis("diffs", bot.diffs)
+
+	//f, err := os.Create("bar.html")
+	//if err != nil {panic(err)}
+	//bar.Render(f)
+
 
 }
