0ea8839ccaff6e80a11b5945516b71aaa000e60b
sanchit <sanchitajmera2017@gmail.com>
2020-07-06 04:57:36 +0100

    improved plotGraph function

--- python/src/candlesticks-sanchit/utils.py
+++ python/src/candlesticks-sanchit/utils.py
@@ -1,21 +1,38 @@
 import main
 import bots
+import xlrd
 import matplotlib.pyplot as plt
 
 
+# global variable
+wb = xlrd.open_workbook("recentAPIdata.xlsx")
+sheet = wb.sheet_by_index(0)
+
+
 def getAsk(currRow):
-    return sheet.cell_value(currRow, 7)
+    res = (sheet.cell_value(currRow, 7))
+
+    if res == "NaN":
+        return getBid(currRow - 1)
+    else:
+        return float(res)
 
 
 def getBid(currRow):
-    return sheet.cell_value(currRow, 7)
+    res = (sheet.cell_value(currRow, 7))
+
+    if res == "NaN":
+        return getBid(currRow - 1)
+    else:
+        return float(res)
 
 
-def plotGraph(ys):
+def plotGraph(ys, bot):
     xs = []
     for i in range(len(ys)):
         xs.append(i)
 
     plt.plot(xs, ys)
+    plt.plot(bot.buys[0], bot.buys[1], 'o', color="orange")
     plt.show()
     plt.close()
