3c099c2041ce9d732af06e000ba6913299c28f0d
sanchit <sanchitajmera2017@gmail.com>
2020-07-06 04:56:34 +0100

    added calcMinMax function

--- python/src/candlesticks-sanchit/candlesticks.py
+++ python/src/candlesticks-sanchit/candlesticks.py
@@ -3,20 +3,36 @@ import bots
 import utils
 
 
-def calcMaxMin(asks, bids)
+def calcMaxMin(asks, bids):
+    LARGEST_INT = 1844674407370955200
+    maxBid = 0
+    maxAsk = 0
+    minBid = LARGEST_INT
+    minAsk = LARGEST_INT
+    for i in range(len(asks)):
+        if asks[i] > maxAsk:
+            maxAsk = asks[i]
+        if asks[i] < minAsk:
+            minAsk = asks[i]
+        if bids[i] > maxBid:
+            maxBid = bids[i]
+        if bids[i] < minBid:
+            minBid = bids[i]
+    return maxAsk, minAsk, maxBid, minBid
 
 
 def calcCandleStick(bot):
     asks = []
     bids = []
     for i in range(bot.tradingPeriod):
-        asks.append(getAsk(bot.currRow))
-        bids.append(getBid(bot.currRow))
+        asks.append(utils.getAsk(bot.currRow - i))
+        bids.append(utils.getBid(bot.currRow - i))
+
     bot.candleStickInfo["openAsk"][2] = asks[0]
     bot.candleStickInfo["openBid"][2] = bids[0]
     bot.candleStickInfo["closeAsk"][2] = asks[bot.tradingPeriod - 1]
-    bot.candleStickInfo["closebid"][2] = bids[bot.tradingPeriod - 1]
+    bot.candleStickInfo["closeBid"][2] = bids[bot.tradingPeriod - 1]
     bot.candleStickInfo["maxAsk"][2],
-    bot.candleStickInfo["maxAsk"][2],
-    bot.candleStickInfo["maxAsk"][2],
-    bot.candleStickInfo["maxAsk"][2] = calcMaxMin(asks, bids)
+    bot.candleStickInfo["minAsk"][2],
+    bot.candleStickInfo["maxBid"][2],
+    bot.candleStickInfo["minBid"][2] = calcMaxMin(asks, bids)
