a31b8546efa952887a44f69dcfaa00de7431250f
Shivam Patel <sap119@ic.ac.uk>
2020-07-11 20:35:45 +0100

    Changed client timeout to 30 secs

---
+++ go/src/live-trading-shivam/apiutils.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	// "fmt"
+	"context"
+	"fmt"
+	luno "github.com/luno/luno-go"
+	"github.com/luno/luno-go/decimal"
+	"time"
+)
+
+func getTickerRequest() (*luno.Client, *luno.GetTickerRequest) {
+	lunoClient := luno.NewClient()
+	lunoClient.SetAuth("b8vuefaayradx", "I7-IM03X1TxuAKbfVv9rYBcywuswyDyENcb2lra8ctA")
+
+	return lunoClient, &luno.GetTickerRequest{Pair: pair}
+}
+
+func getCurrBid() decimal.Decimal {
+	res, err := client.GetTicker(context.Background(), reqPointer)
+	if err != nil {
+		panic(err)
+	}
+	return res.Bid
+}
+
+func getCurrAsk() decimal.Decimal {
+	res, err := client.GetTicker(context.Background(), reqPointer)
+	if err != nil {
+		panic(err)
+	}
+	return res.Ask
+}
+
+func getAsset(currency string) decimal.Decimal {
+	balancesReq := luno.GetBalancesRequest{}
+	balances, err := client.GetBalances(context.Background(), &balancesReq)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, accBalance := range balances.Balance {
+		if accBalance.Asset == currency {
+			return accBalance.Balance
+		}
+	}
+
+	panic("Cannot retrieve account balance")
+}
+
+func getTicker() (decimal.Decimal, decimal.Decimal) {
+	res, err := client.GetTicker(context.Background(), reqPointer)
+	if err != nil {
+		fmt.Println(err)
+		time.Sleep(time.Minute)
+		return getTicker()
+	}
+	return res.Ask, res.Bid
+}
