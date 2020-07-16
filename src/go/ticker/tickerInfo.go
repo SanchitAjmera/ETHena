fa2cf53527a7e13c02c6663f8db9718f94bc8839
Manuj Mishra <manujmishra2000@gmail.com>
2020-07-16 22:19:20 +0100

    Fixed for loop ordering issue with getTickerRequest

--- src/go/ticker/tickerInfo.go
+++ src/go/ticker/tickerInfo.go
@@ -45,45 +45,44 @@ func main(){
 
   pairs := []string{"XBTGBP","ETHXBT","XRPXBT","XRPZAR","BCHXBT","LTCXBT","XBTZAR"}
   columns := []string{"A","B","C","D","E","F","G","H","I","J","K","L","M","N"}
-  client, reqPointer = getTickerRequest(pair)
   client.SetTimeout(time.Minute)
 
-  for hour:=0 ;hour < 24; hour++{
-    f := excelize.NewFile()
-    f.SetCellValue("Sheet1", "A1", "XBTGBP bid")
-    f.SetCellValue("Sheet1", "B1", "XBTGBP ask")
-    f.SetCellValue("Sheet1", "C1", "ETHXBT bid")
-    f.SetCellValue("Sheet1", "D1", "ETHXBT ask")
-    f.SetCellValue("Sheet1", "E1", "XRPXBT bid")
-    f.SetCellValue("Sheet1", "F1", "XRPXBT ask")
-    f.SetCellValue("Sheet1", "G1", "XRPZAR bid")
-    f.SetCellValue("Sheet1", "H1", "XRPZAR ask")
-    f.SetCellValue("Sheet1", "I1", "BCHXBT bid")
-    f.SetCellValue("Sheet1", "J1", "BCHXBT ask")
-    f.SetCellValue("Sheet1", "K1", "LTCXBT bid")
-    f.SetCellValue("Sheet1", "L1", "LTCXBT ask")
-    f.SetCellValue("Sheet1", "M1", "XBTZAR bid")
-    f.SetCellValue("Sheet1", "N1", "XBTZAR ask")
+  for index,pair := range pairs {
+    client, reqPointer = getTickerRequest(pair)
+    for hour:=0 ;hour < 24; hour++{
+      f := excelize.NewFile()
+      f.SetCellValue("Sheet1", "A1", "XBTGBP bid")
+      f.SetCellValue("Sheet1", "B1", "XBTGBP ask")
+      f.SetCellValue("Sheet1", "C1", "ETHXBT bid")
+      f.SetCellValue("Sheet1", "D1", "ETHXBT ask")
+      f.SetCellValue("Sheet1", "E1", "XRPXBT bid")
+      f.SetCellValue("Sheet1", "F1", "XRPXBT ask")
+      f.SetCellValue("Sheet1", "G1", "XRPZAR bid")
+      f.SetCellValue("Sheet1", "H1", "XRPZAR ask")
+      f.SetCellValue("Sheet1", "I1", "BCHXBT bid")
+      f.SetCellValue("Sheet1", "J1", "BCHXBT ask")
+      f.SetCellValue("Sheet1", "K1", "LTCXBT bid")
+      f.SetCellValue("Sheet1", "L1", "LTCXBT ask")
+      f.SetCellValue("Sheet1", "M1", "XBTZAR bid")
+      f.SetCellValue("Sheet1", "N1", "XBTZAR ask")
 
-    fmt.Println("Hour: ", hour) // For nohup test (printing to terminal)
+      fmt.Println("Hour: ", hour) // For nohup test (printing to terminal)
 
-    row := 2
-    for i:=0 ; i< 60; i ++ {
-      for index,pair := range pairs {
-        ask, bid, _ = getTicker()
-        cell1 := columns[index*2] + strconv.Itoa(row)
-        cell2 := columns[index*2+1] + strconv.Itoa(row)
-        f.SetCellValue("Sheet1", cell1, bid.String())
-        f.SetCellValue("Sheet1", cell2, ask.String())
-        time.Sleep(5*time.Second)
+      row := 2
+      for i:=0 ; i< 60; i ++ {
+          ask, bid, _ = getTicker()
+          cell1 := columns[index*2] + strconv.Itoa(row)
+          cell2 := columns[index*2+1] + strconv.Itoa(row)
+          f.SetCellValue("Sheet1", cell1, bid.String())
+          f.SetCellValue("Sheet1", cell2, ask.String())
+          time.Sleep(5*time.Second)
+        row +=1
+        time.Sleep(25*time.Second)
       }
-      row +=1
-      time.Sleep(25*time.Second)
-    }
 
-    if err := f.SaveAs("tickerData"+strconv.Itoa(hour)+".xlsx"); err != nil {
-      println(err.Error())
+      if err := f.SaveAs("tickerData"+strconv.Itoa(hour)+".xlsx"); err != nil {
+        println(err.Error())
+      }
     }
   }
-
 }
