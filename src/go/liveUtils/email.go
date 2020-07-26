fb8936688a12a5f82752470899625cccbcb78067
sanchit <sanchitajmera2017@gmail.com>
2020-07-26 03:38:49 +0100

    complete implementation of emailing graphed data

--- src/go/liveUtils/email.go
+++ src/go/liveUtils/email.go
@@ -4,6 +4,7 @@ import (
     "fmt"
     "github.com/luno/luno-go/decimal"
     "gopkg.in/gomail.v2" // perform go get <this> when initialising servers
+//    "time"
 )
 
 
@@ -18,27 +19,30 @@ func Email(action string, yield decimal.Decimal) {
       messageStr = "Daily update: "
       if yield.Sign() == 1 {
         messageStr += "PROFIT! £" + yield.String()
-      } else if yield.Sign() == 1{
-        yield.Mul(decimal.NewFromFloat64(-1,8))
-        messageStr += "LOSS! £" + yield.String()
+      } else if yield.Sign() == -1{
+        yieldAbs := yield.Sub((decimal.NewFromFloat64(2,2)).Mul(yield))
+        messageStr += "LOSS! £" + yieldAbs.String()
       } else {
         messageStr += "FLAT! £" + yield.String()
       }
+//      fileName := time.Now().Format("2006-01-02")
       m.Attach("../main/graph.png")
-      messageStr += yield.String()
+//      m.Attach("../main/" + fileName + ".xlsx")
     case "START":
       messageStr = "NEWS! Your bot has begun trading"
   }
 
 	m.SetHeader("Subject", messageStr)
-	m.SetBody("text/html", ".")
+	m.SetBody("text/html", "")
 
 	d := gomail.NewDialer("smtp.gmail.com", 587, "profit.profit.profit.icl@gmail.com", "Password123??")
 
 	// Send the email to Bob, Cora and Dan.
 	if err := d.DialAndSend(m); err != nil {
-		panic(err)
+		fmt.Println("ERROR! graph.png doesn't exist ",err)
+    fmt.Println("Update email NOT successfully sent")
+    return
 	}
 
-  fmt.Println("Update email successfully sent!")
+  fmt.Println("Update email successfully sent")
 }
