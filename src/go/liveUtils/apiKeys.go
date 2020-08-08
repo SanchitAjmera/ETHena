5e3c6e49032153fd0f69de5304982f5cd971d930
sanchit <sanchitajmera2017@gmail.com>
2020-08-08 17:38:26 +0100

    added temporary API details storage

---
+++ src/go/liveUtils/apiKeys.go
@@ -0,0 +1,17 @@
+package liveUtils
+
+func InitialiseKeys(){
+  ApiKeys = map[string]([]string){
+    "SHIVAM" : []string{
+      "dw5vev3azw6af",
+      "40COv0KPwD0L3LYPLNppgDESWxTkKmhD5AsrdyaDGAA",
+    },
+    "SANCHIT" : []string{
+      "g8ypyturf3a5s",
+      "BZIkiT2IgPhIFMRdBMvKIadmNP_MtksMvHwCaNfqVgE",
+    },
+    "MANUJ" : []string{},
+    "LUQMAN" : []string{},
+    "DEVAM" : []string{},
+  }
+}
