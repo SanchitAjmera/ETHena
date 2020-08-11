f58c614d2c01addc5518bdbf1a0beb5b7bc63631
sanchit <sanchitajmera2017@gmail.com>
2020-08-11 05:05:47 +0100

    implemented GUI graphics

--- src/go/server/testFile.go
+++ src/go/server/testFile.go
@@ -1,6 +1,7 @@
 package main
 
 import(
+  "fmt"
   "syscall/js"
   "strconv"
 )
@@ -29,9 +30,22 @@ func subtract(this js.Value, i []js.Value) interface{} {
     return int1 - int2
 }
 
+func Run(this js.Value, i []js.Value) interface{} {
+  keyID := js.Global().Get("document").Call("getElementById", i[0].String()).Get("value").String()
+  key := js.Global().Get("document").Call("getElementById", i[1].String()).Get("value").String()
+  email := js.Global().Get("document").Call("getElementById", i[2].String()).Get("value").String()
+  rsi := js.Global().Get("document").Call("getElementById", i[3].String()).Get("value").String()
+  macd := js.Global().Get("document").Call("getElementById", i[4].String()).Get("value").String()
+  candlesticks := js.Global().Get("document").Call("getElementById", i[5].String()).Get("value").String()
+  js.Global().Get("document").Call("getElementById", i[6].String()).Set("value", 2)
+  fmt.Println(keyID, key, email, rsi, macd, candlesticks)
+  return 1
+}
+
 func registerCallbacks() {
     js.Global().Set("add", js.FuncOf(add))
     js.Global().Set("subtract", js.FuncOf(subtract))
+    js.Global().Set("Run", js.FuncOf(Run))
 }
 
 func main() {
