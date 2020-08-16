2f38707e7185080ba4ab003dc3d289bc61b9f827
Devam-Savjani <61628992+Devam-Savjani@users.noreply.github.com>
2020-08-16 14:50:12 +0100

    Removed deprecated API Keys

--- src/go/utils/apiKeys.go
+++ src/go/utils/apiKeys.go
@@ -3,22 +3,9 @@ package Utils
 //InitialiseKeys is
 func InitialiseKeys() {
 	ApiKeys = map[string]([]string){
-		"SHIVAM": []string{
-			"dw5vev3azw6af",
-			"40COv0KPwD0L3LYPLNppgDESWxTkKmhD5AsrdyaDGAA",
-		},
-		"SANCHIT": []string{
-			"g8ypyturf3a5s",
-			"BZIkiT2IgPhIFMRdBMvKIadmNP_MtksMvHwCaNfqVgE",
-		},
-		"MANUJ": []string{
-			"jt8tkjzekh52e",
-			"KUKkerlv1-1GYbSaHrFa_I1uYzTZPY8AqJpOH2XyGck",
-		},
-		"LUQMAN": []string{},
-		"DEVAM": []string{
-			"mggh7nx5v5vzn",
-			"DiHrN4Lqu27eCajdCTBEKU4H-oIFAFR4_k1eRlx5Kho",
+		"NAME": []string{
+			"KEY_ID",
+			"SECRET",
 		},
 	}
 }
